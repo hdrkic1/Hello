d1a3984a07684c89659fff95ca468edbf85cfe2f
Haris <hdrkic1@etf.unsa.ba>
2021-03-19 20:22:43 +0100

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,9 @@
+package sample;
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+    public void seashells(ActionEvent actionEvent) {
+        System.out.println("Hello World!");
+    }
+}
